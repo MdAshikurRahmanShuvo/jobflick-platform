fae2a8371b63c428aa4f1860fc267d9afc6c6501
Md. Ashikur Rahman Shuvo <shuvo15-5633@diu.edu.bd>
2025-11-21 16:03:11 +0600

    navbar added in contact page

--- jobflick/wsgi.py
+++ jobflick/wsgi.py
@@ -1,16 +1,11 @@
-"""
-WSGI config for jobflick project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
+import sys
 import os
 
+path = '/home/YOUR_USERNAME/jobflick-platform'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobflick.settings")
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobflick.settings')
-
 application = get_wsgi_application()
